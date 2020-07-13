1fc81c4fc3062928382e646e6e6edcc1b4970703
Akhil Kumar <akhil.kumar@quincus.com>
2020-07-13 22:06:50 +0530

    cross_origin-set-to_null_perfectly_added

--- backend/API.py
+++ backend/API.py
@@ -62,7 +62,7 @@ app.secret_key = 'akhilpandey'
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allow-Origin', "null")
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
